d1adbddb18b8bee8b6c55f6760427d73f094ca75
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-14 23:00:07 -0700

    begin adding prompts to array

--- index.js
+++ index.js
@@ -1,2 +1,63 @@
 const fs = require('fs');
 const inquirer = require('fs');
+//prompting user for information to be written to the new file.
+inquirer.prompt([ 
+    {
+        type: 'input', 
+        message: 'What is the title of this project?',
+        name: 'title'
+
+    },
+    {
+        type: 'input', 
+        message: 'Please enter a description of your project.',
+        name: 'description'
+
+    },
+    {
+        type: 'input', 
+        message: 'What installations were required for this project?',
+        name: 'installation'
+
+    },
+    {
+        type: 'input', 
+        message: '',
+        name: 'usage'
+
+    },
+    {
+        type: 'input', 
+        message: '',
+        name: 'license'
+
+    },
+    {
+        type: 'input', 
+        message: '',
+        name: 'contributing'
+
+    },
+    {
+        type: 'input', 
+        message: '',
+        name: 'tests'
+
+    },
+    {
+        type: 'input', 
+        message: '',
+        name: 'questions'
+
+    },
+
+
+
+
+
+
+
+
+
+
+])
